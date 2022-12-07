3c7538928d44712ba4b882b9776da203a119294d
TSUKAMOTO Tetsuo <tsukamoto@iret.co.jp>
2022-12-07 22:06:45 +0900

    init

---
+++ main.py
@@ -0,0 +1,52 @@
+import datetime
+from bs4 import BeautifulSoup
+import requests
+import feedgenerator
+import csv
+
+feed_file = open('feed.csv')
+feeds = csv.reader(feed_file)
+
+index = open('feeds/index.html', 'w')
+index.write('<!DOCTYPE html><html><body><ul>')
+
+for feed in feeds:
+    print(feed)
+    comics_url = "https://yanmaga.jp/comics/" + feed[1]
+    comics = requests.get(comics_url).text
+
+    soup = BeautifulSoup(comics, 'html.parser')
+
+    detail_img = soup.find('div', class_="detailv2-thumbnail-image").img
+    print(detail_img.get('alt'))
+    print(detail_img.get('src'))
+
+    rss = feedgenerator.Atom1Feed(
+        title=detail_img.get('alt'),
+        link=comics_url,
+        description='',
+        language="ja",
+        image=detail_img.get('src')
+    )
+
+    for episode in soup.find_all('div', class_="mod-episode-public"):
+        if episode.find('span', class_="mod-episode-point--free") is None:
+            continue
+
+        # print(episode)
+        href = episode.a.get('href')
+
+        rss.add_item(
+            unique_id=href.rsplit('/', 1)[1],
+            title=episode.find('p', class_="mod-episode-title").text,
+            link="https://yanmaga.jp" + href,
+            description="",
+            pubdate=datetime.datetime.strptime(episode.find('time', class_="mod-episode-date").text + ' 00:00:00+0900', '%Y/%m/%d %H:%M:%S%z')
+        )
+
+    with open('feeds/' + feed[0] + '.xml', 'w') as fp:
+        rss.write(fp, 'utf-8')
+
+    index.write('<li><a href="{href}">{title}</a></li>'.format(href=feed[0] + '.xml', title=feed[1]))
+
+index.write('</ul></body></html>')
